0f45b6dbdcc698197db1e3560a9b7ac720e92edb
Jagath Jai Kumar <jagath.jaikumar@gmail.com>
2022-12-03 20:35:00 -0500

    finished day 3

---
+++ 3/solution.py
@@ -0,0 +1,45 @@
+import os
+from typing import List
+
+input_path = os.path.join(os.path.dirname(__file__), "input.txt")
+
+alphabet_reference = "*abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+with open(input_path) as f:
+    sacks = f.read().split("\n")
+
+
+def find_common_char_in_strings(inputs: List[str]) -> str:
+    # length 1, assumes common char exists
+    return list(set.intersection(*map(set, inputs)))[0]
+
+
+def part_1():
+
+    summ = sum(
+        [
+            alphabet_reference.index(
+                find_common_char_in_strings(
+                    # split sack into equal size compartments
+                    [sack[: len(sack) // 2], sack[len(sack) // 2 :]]
+                )
+            )
+            for sack in sacks
+        ]
+    )
+
+    print(summ)
+
+
+def part_2():
+    # split sacks into groups of 3
+    triplets = [sacks[i : i + 3] for i in range(0, len(sacks), 3)]
+
+    summ = sum(
+        [
+            alphabet_reference.index(find_common_char_in_strings(triplet))
+            for triplet in triplets
+        ]
+    )
+
+    print(summ)
