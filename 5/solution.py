36ee9b494e519bd14ec9aa66d82b3783b01f38cb
Jagath Jai Kumar <jjaikumar@google.com>
2022-12-05 10:26:11 -0500

    completed day 5

--- 5/solution.py
+++ 5/solution.py
@@ -4,16 +4,16 @@ import re
 
 input_path = os.path.join(os.path.dirname(__file__), "input.txt")
 
-def part_1():
+def startup():
   with open(input_path) as f:
     everything = f.read().split("\n")
-  
+
   instruction_split_line = everything.index("")
   containers_line = instruction_split_line - 1
   n_containers = int(list(filter(None, everything[containers_line].split(" ")))[-1])
-  
+
   containers = [[] for _ in range(n_containers)]
-  
+
   for i in range(containers_line-1, -1, -1):
     container_line = re.findall('....',everything[i] + " ")
     for index, container in enumerate(container_line):
@@ -22,6 +22,11 @@ def part_1():
         containers[index].append(container)
 
   instructions = everything[instruction_split_line+1:]
+
+  return instructions, containers
+
+def part_1():
+  instructions, containers = startup()
 
   for instruction in instructions:
     _ = instruction.split(" ")
@@ -32,7 +37,17 @@ def part_1():
   return "".join([stack[-1][1] for stack in containers])
 
 def part_2():
-  pass
+  instructions, containers = startup()
 
-  return ""
+  for instruction in instructions:
+    _ = instruction.split(" ")
+    quantity_, from_, to_ = int(_[1]), int(_[3])-1, int(_[5])-1
+
+    to_move = []
+    for i in range(quantity_):
+      to_move.append(containers[from_].pop())
+    to_move.reverse()
+    containers[to_].extend(to_move)
+
+  return "".join([stack[-1][1] for stack in containers])
     
