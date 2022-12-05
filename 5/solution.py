bc20b1fc7750e4faa37ed5af3a3100337ba3a2bc
Jagath Jai Kumar <jjaikumar@google.com>
2022-12-05 10:17:37 -0500

    finished part 1

---
+++ 5/solution.py
@@ -0,0 +1,38 @@
+# Advent of Code, Day 5
+import os
+import re
+
+input_path = os.path.join(os.path.dirname(__file__), "input.txt")
+
+def part_1():
+  with open(input_path) as f:
+    everything = f.read().split("\n")
+  
+  instruction_split_line = everything.index("")
+  containers_line = instruction_split_line - 1
+  n_containers = int(list(filter(None, everything[containers_line].split(" ")))[-1])
+  
+  containers = [[] for _ in range(n_containers)]
+  
+  for i in range(containers_line-1, -1, -1):
+    container_line = re.findall('....',everything[i] + " ")
+    for index, container in enumerate(container_line):
+      container = container.strip()
+      if container:
+        containers[index].append(container)
+
+  instructions = everything[instruction_split_line+1:]
+
+  for instruction in instructions:
+    _ = instruction.split(" ")
+    quantity_, from_, to_ = int(_[1]), int(_[3])-1, int(_[5])-1
+    for i in range(quantity_):
+      containers[to_].append(containers[from_].pop())
+
+  return "".join([stack[-1][1] for stack in containers])
+
+def part_2():
+  pass
+
+  return ""
+    
